766c13989d007f6fcaa9371abf8ed4b24741862a
VictorAfro <victorgit82@gmail.com>
2022-12-08 15:04:36 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print out sizes of data types in c
+ * code by victor
+ * Return: 0
+ */
+int main(void)
+{
+	printf("size of a char: %i byte(s)\n", sizeof(char));
+	printf("size of an int: %i byte(s)\n", sizeof(int));
+	printf("size of a long int %i byte(s)\n", sizeof(long int));
+	printf("size of a lon long int %i byte(s)\n", sizeof(float));
+	return (0);
+}
