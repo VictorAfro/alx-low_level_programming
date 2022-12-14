f20a25fe883ea49fbdf16737ed55cd6fe57a0847
VictorAfro <victorgit82@gmail.com>
2022-12-14 09:26:47 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
- * main - print _putchar
- * is written by afrovictor
- * Return: 0
+ *
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	_putchar('_');
 	_putchar('p');
-	_putchar('u');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
@@ -16,6 +17,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
-}
 
+	return (0);
+
+}
