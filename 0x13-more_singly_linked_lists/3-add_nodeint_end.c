8d1811587f91a4bca05bbb4f1f9b26571e49da45
VictorAfro <victorgit82@gmail.com>
2023-02-03 03:14:39 -0800

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,7 +21,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->n = n;
 	new->next = NULL;
 
-	if (*head == NULL);
+	if (*head == NULL)
+		*head = new;
+
 	else
 	{
 		last = *head;
